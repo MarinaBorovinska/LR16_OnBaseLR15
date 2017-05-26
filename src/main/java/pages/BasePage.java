a065808ddd1c96ed53b272e534efa29bed6ad211
Марина <marina.borovinska@gmail.com>
2017-05-26 11:26:20 +0300

    added base version

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,159 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.RemoteDriverManager;
+
+public class BasePage {
+
+    final static Logger logger = Logger.getLogger(BasePage.class);
+
+    protected String baseURL = "http://soft.it-hillel.com.ua:8080";
+    protected WebDriver driver;
+    private int timeOutInSeconds = 10;
+
+    protected BasePage() {
+        this.driver = RemoteDriverManager.getDriver();
+    }
+
+    protected void waitToBePresent(By locator) {
+        logger.info("WAIT ELEMENT TO BE PRESENT: " + locator);
+        (new WebDriverWait(driver, timeOutInSeconds))
+                .until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    /**
+     * @param locator
+     * @param timeOutInSeconds
+     */
+    protected void waitToBePresentIn(By locator, int timeOutInSeconds) {
+        logger.info("WAIT ELEMENT TO BE PRESENT: " + locator + " in " + timeOutInSeconds + " seconds");
+        (new WebDriverWait(driver, timeOutInSeconds))
+                .until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    /**
+     * @param locator
+     */
+
+    protected void waitToBePresentAndClick(By locator) {
+        logger.info("WAIT ELEMENT TO BE PRESENT AND CLICK: " + locator);
+        WebElement element = null;
+
+        try {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.click();
+        } catch (StaleElementReferenceException ignored) {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.click();
+        }
+
+    }
+
+    protected void waitToBePresentAndSendKeys(By locator, String keys) {
+        logger.info("WAIT ELEMENT TO BE PRESENT AND SEND KEYS: " + locator);
+
+        WebElement element = null;
+
+        try {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.sendKeys(keys);
+        } catch (StaleElementReferenceException ignored) {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.sendKeys(keys);
+        }
+
+    }
+
+    protected void waitToBePresentAndSendSubmit(By locator) {
+        logger.info("WAIT ELEMENT TO BE PRESENT AND SUBMIT: " + locator);
+        WebElement element = null;
+
+        try {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.submit();
+        } catch (StaleElementReferenceException ignored) {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            element.submit();
+        }
+
+    }
+
+    protected boolean waitToBePresentAndContainsText(By locator, String text) {
+        logger.info("WAIT ELEMENT TO BE PRESENT AND CONTAINS TEXT: " + locator);
+        WebElement element = null;
+
+        try {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+
+            String result = element.getText();
+
+            if (text.equals(result)) {
+                return true;
+            } else {
+                return false;
+            }
+
+
+
+        } catch (StaleElementReferenceException ignored) {
+            element = (new WebDriverWait(driver, timeOutInSeconds)).
+                    until(ExpectedConditions.presenceOfElementLocated(locator));
+            String result = element.getText();
+
+            if (text.equals(result)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+
+    protected void scrollDown() {
+
+    }
+
+    protected void scrollUp() {
+
+    }
+
+    protected void scrollUpOn(int x) {
+        logger.info("SCROLL UP ON: " + x + " pixels");
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,-" + x + ")", "");
+    }
+
+    protected void scrollDownOn(int x) {
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0," + x + ")", "");
+    }
+
+//    protected WebElement findElement(By locator) {
+//        return driver.findElement(locator);
+//    }
+
+
+    protected boolean isOnThePage(String expectedURL) {
+
+        String currentURL = driver.getCurrentUrl();
+        boolean result = expectedURL.equals(currentURL);
+        if (result == true) {
+            logger.info("IS ON THE PAGE: " + expectedURL);
+        } else {
+            logger.info("YOU ARE NOT ON THE PAGE: " + expectedURL);
+            logger.info("YOU ARE ON THE PAGE: " + driver.getCurrentUrl());
+        }
+        return result;
+
+    }
+
+}
---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,68 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.*;
+
+import static org.testng.Assert.assertEquals;
+
+public class SimpleTest {
+
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+
+    @Test(groups = {"functional"})
+    public void subTaskCRUD() throws InterruptedException {
+
+        String parentIssue = "QAAUT-224";
+        String subTaskSummary = "Marina test";
+
+        //Login
+
+        LoginPage loginPage = new LoginPage();
+        NewIssuePage newIssuePage = new NewIssuePage();
+        HeaderPage headerPage = new HeaderPage();
+        DashBoardPage dashBoardPage = new DashBoardPage();
+        IssuePage issuePage = new IssuePage();
+
+        loginPage.open();
+        assertEquals(loginPage.isOnThePage(), true);
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+        loginPage.clickLogin();
+
+        assertEquals(dashBoardPage.isOnThePage(), true);
+
+        //Create new sub-task
+
+        headerPage.search(parentIssue);
+
+        issuePage.openNewSubTask();
+        newIssuePage.fillSummary(subTaskSummary);
+        newIssuePage.clickSubmitButton();
+
+        // TODO assert for sub-task title
+        assertEquals(issuePage.isSubTaskSummaryPresent(subTaskSummary), true);
+
+        // TODO assert for sub-task number
+        // TODO assert for sub-task assignee
+        // TODO assert for sub-task status
+
+        //Delete new sub-task
+        issuePage.openSubtask();
+        issuePage.shouldSeeSuccessPopUp();
+        issuePage.clickMoreButton();
+        issuePage.clickDeleteListItem();
+        issuePage.deleteSubTask();
+
+        // TODO assert that sub-task title doesn't exist
+
+    }
+
+    @Test(groups = {"functional"})
+    public void subTaskCommentCRUD() throws InterruptedException {
+
+
+    }
+}
